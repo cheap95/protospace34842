f0ca6309d71dbb05ee00f50de7ba5af883813e08
cheap95 <cheap95@gmail.com>
2021-03-15 18:05:56 +0900

    プロタイプ新規投稿

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,19 +1,32 @@
 class PrototypesController < ApplicationController
   def index
 
-    
+
     
   end
   def new
-
+    @prototype = Prototype.new
     
   end
 
   def create
+    if Prototype.create(prototype_params)
+      riderect_to root_path
+
+    else
+      render :new
+      
+    end
+
     
   end
 
   private
+
+  def prototype_params
+    params.require(:prototype).permit(:image).merge(user_id: current_user.id)
+
+  end
 
 
 end
